3771885a4339c209e964249004bd5a13d86e76cb
ditinagrawal <ditin.agrawal05@gmail.com>
2025-08-20 20:46:38 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import gsap from "gsap";
+import { ScrollTrigger, SplitText } from "gsap/all";
+import React from "react";
+
+gsap.registerPlugin(ScrollTrigger, SplitText);
+
+const App = () => {
+  return <div className="flex-center h-[100vh]">App</div>;
+};
+
+export default App;
